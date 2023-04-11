9292c22c659c83760986fdddbe1b721b7d263a76
varshinysathish <98300016+varshinysathish@users.noreply.github.com>
2023-04-11 16:33:22 +0800

    added weather app structure

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Singapore" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,84 @@
 import React from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
-    );
-  }
-  let apiKey = "9ece65da0f72264bfe679ddf770d6e93";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "Singapore",
+    temperature: 28,
+    date: "Tuesday 15:54",
+    description: "Broken clouds",
+    imgUrl: "http://openweathermap.org/img/wn/04d@2x.png",
+    humidity: 76,
+    wind: 8,
+  };
+
   return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
+    <div className="Weather">
+      <form id="search-form" className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control search-button"
+              id="city-input"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <button>Search</button>
+          </div>
+        </div>
+        <br />
+      </form>
+      <div className="overview">
+        <h1 id="city">{weatherData.city}</h1>
+        <ul>
+          <li className="dt-desc">
+            Last updated:{" "}
+            <span className="dt-desc" id="date">
+              {weatherData.date}
+            </span>
+          </li>
+          <li className="dt-desc description" id="description">
+            {weatherData.description}
+          </li>
+        </ul>
+        <br />
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <div className="float-left">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                id="icon"
+                className="float-left"
+              />
+              <strong id="temperature">{weatherData.temperature}</strong>
+              <span className="units"> °C </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>
+              Humidity:
+              <span className="humidity-wind">
+                <span id="humidity"></span> {weatherData.humidity}%
+              </span>
+            </li>
+            <li>
+              Wind:
+              <span className="humidity-wind">
+                <span id="wind"></span> {weatherData.humidity} km/h
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
