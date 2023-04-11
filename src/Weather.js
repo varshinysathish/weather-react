07f4ec98c3a27f7a429d6c895d4cb3b4674f4624
varshinysathish <98300016+varshinysathish@users.noreply.github.com>
2023-04-11 18:24:55 +0800

    updated css

--- src/Weather.js
+++ src/Weather.js
@@ -14,71 +14,79 @@ export default function Weather() {
   };
 
   return (
-    <div className="Weather">
-      <form id="search-form" className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control search-button"
-              id="city-input"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <button>Search</button>
-          </div>
-        </div>
-        <br />
-      </form>
-      <div className="overview">
-        <h1 id="city">{weatherData.city}</h1>
-        <ul>
-          <li className="dt-desc">
-            Last updated:{" "}
-            <span className="dt-desc" id="date">
-              {weatherData.date}
-            </span>
-          </li>
-          <li className="dt-desc description" id="description">
-            {weatherData.description}
-          </li>
-        </ul>
-        <br />
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <div className="float-left">
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                id="icon"
-                className="float-left"
+    <div className="Weather-React">
+      <div className="Weather">
+        <form id="search-form" className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control search-button"
+                id="city-input"
+                autoComplete="off"
               />
-              <strong id="temperature">{weatherData.temperature}</strong>
-              <span className="units"> °C </span>
+            </div>
+            <div className="col-3">
+              <button>Search</button>
             </div>
           </div>
-        </div>
-        <div className="col-6">
+          <br />
+        </form>
+        <div className="overview">
+          <h1 id="city">{weatherData.city}</h1>
           <ul>
-            <li>
-              Humidity:
-              <span className="humidity-wind">
-                <span id="humidity"></span> {weatherData.humidity}%
+            <li className="dt-desc">
+              Last updated:{" "}
+              <span className="dt-desc" id="date">
+                {weatherData.date}
               </span>
             </li>
-            <li>
-              Wind:
-              <span className="humidity-wind">
-                <span id="wind"></span> {weatherData.humidity} km/h
-              </span>
+            <li className="dt-desc description" id="description">
+              {weatherData.description}
             </li>
           </ul>
+          <br />
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <div className="float-left">
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  id="icon"
+                  className="float-left"
+                />
+                <strong id="temperature">{weatherData.temperature}</strong>
+                <span className="units"> °C </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity:
+                <span className="humidity-wind">
+                  <span id="humidity"></span> {weatherData.humidity}%
+                </span>
+              </li>
+              <li>
+                Wind:
+                <span className="humidity-wind">
+                  <span id="wind"></span> {weatherData.humidity} km/h
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+      <footer>
+        <a href="https://github.com/varshinysathish/weather-react">
+          Open-source code
+        </a>
+        , by Varshiny Sathishkumar
+      </footer>
     </div>
   );
 }
